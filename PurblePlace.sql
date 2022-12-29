778c18718f6bf0013ec00ceaa8ccf83ee8d13579
LoorPaulina <paumloor@espol.edu.ec>
2022-12-29 11:00:09 -0500

    Usuario

--- PurblePlace.sql
+++ PurblePlace.sql
@@ -1,2 +1,16 @@
-Create database PurblePlace;
-Use PurblePlace;
+/*Create database PurblePlace;
+Use PurblePlace;*/
+
+USE PurblePlace;
+
+CREATE TABLE USUARIO
+(
+codigo varchar(5) primary key not null,
+nombres varchar(10),
+apellidos varchar(10),
+nombre_Usuario varchar(10),
+correo varchar(20),
+clave varchar(7)
+);
+
+

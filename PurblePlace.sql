89e5dea460600657aee6ec544f26b4e401d6e873
TaizRuiz <dafnetaizruiz@gmail.com>
2022-12-29 20:14:03 -0500

    tablausuarios

--- PurblePlace.sql
+++ PurblePlace.sql
@@ -1,2 +1,9 @@
-Create database PurblePlace;
+/*Create database PurblePlace;*/
 Use PurblePlace;
+create table usuario(
+codigoUsuario int not null primary key auto_increment,
+nombres varchar(50),
+apellidos varchar(50),
+nombreUsuario varchar(15),
+password varchar(15)
+);

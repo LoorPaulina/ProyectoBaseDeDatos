f0f2d4f90b33b2c1fc64db998e18372538762816
TaizRuiz <dafnetaizruiz@gmail.com>
2023-01-08 22:55:22 -0500

    almacenado log in

--- PurblePlace.sql
+++ PurblePlace.sql
@@ -1,5 +1,5 @@
 
-USE PurblePlace;
+/**USE PurblePlace;
 drop table if exists USUARIO;
 CREATE TABLE USUARIO
 (
@@ -13,10 +13,9 @@ clave varchar(7)
 drop table if exists proovedor;
 create table proovedor(
 ruc varchar(10) primary key not null,
-codigoUsuario int not null ,
 nombre varchar(50),
 telefono varchar(10),
-constraint c1 foreign key(codigoUsuario) references USUARIO(codigo)
+
 
 );
 drop table if exists materiaPrima;
@@ -51,4 +50,66 @@ idReceta int not null,
 cantidadNecesaria double not null,
 constraint c4 foreign key (idMateriaPrima) references materiaPrima(Codigo),
 constraint c5 foreign key (idReceta) references receta(codigo)
-);
+);**/
+
+/**insert into usuario (nombres, apellidos, nombre_Usuario,correo,clave) values 
+("Gisella","Perez","Gperez","gisella@hotmail.com","G12345");**/
+
+/**insert into proovedor(ruc,nombre,telefono) values 
+("0952959484","Industrias Trigo S.A","0981383239"),
+("0981383239","Inalecsa S.A","0952959484"),
+("0914463815","Megamaxi S.A","0981383239");**/
+/**insert into materiaprima (nombreProducto,unidadesMedida) values
+("chocolate blanco","kg"),
+("chocolate negro","kg"),
+("harina de trigo","kg"),
+("cafe","kg"),
+("manjar","kg");**/
+select *
+from proovedor_materia_prima;
+/**trigger para que calcule el costo unitario despues de un ingreso**/
+/**drop trigger if exists inProovedorMateria;
+create trigger inProovedorMateria before insert on proovedor_materia_prima for each row
+set new.costo_unitario_producto = new.costoxProovedor/new.unidadesRecibidas;
+**/
+/**insert into proovedor_materia_prima(codigo_materiaPrima,codigo_Proovedor,costoxProovedor,fechaCaducidad,unidadesRecibidas)
+values(3,"0952959484",100,"2023-02-02",20);*//
+/*(4,"0981383239",100,"2023-02-02",18),
+(5,"0914463815",150,"2023-04-02",15),
+(1,"0914463815",550,"2023-02-02",100),
+(1,"0952959484",50,"2022-02-02",20);*/
+/*FUNCION QUE VERIFICA SI EL USUARIO ES CORRECTO*/
+delimiter //
+create function logIn (nomUser varchar(50),passW varchar(50))
+returns boolean
+READS SQL DATA
+DETERMINISTIC
+begin
+declare numero int;
+declare logeado boolean;
+select count(codigo) into numero from usuario where nombre_Usuario=nomUser and clave=passW;
+if numero >=1 then 
+set logeado=true;
+else
+set logeado=false;
+end if;
+return logeado;
+end//
+delimiter ;
+/*PRUEBA DE LA FUNCION*/
+select logIn("Gperez","G12345");
+drop procedure if exists almacenadoLogIn;
+delimiter //
+create procedure almacenadoLogIn ( in nomUser varchar(50),in passWord varchar(50),
+out logeado boolean )
+
+begin 
+start transaction;
+    select logIn(nomUser,passWord) into logeado;
+	
+end
+//
+delimiter ;
+select * from usuario;
+call almacenadoLogIn("Gperez","G1234",@logeado);
+select @logeado;
